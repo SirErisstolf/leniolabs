a9f95a12752d692403bdd4625bad1576d3d43ea8
Erick Bahamondes <ebahamondes@comscore.com>
2019-10-16 13:05:44 -0300

    read API and list members 20 min

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
-import logo from './logo.svg';
+
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+//  Switch,
   Route,
-  Link
+//  Link
 } from "react-router-dom";
 
 class App extends Component {
--- challenge/src/Home.js
+++ challenge/src/Home.js
@@ -1,8 +1,37 @@
 import React, {Component} from 'react';
 export default class Home extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {members: [],test :"1"};
+  }
+  
+  componentDidMount() {
+    fetch('https://api.propublica.org/congress/v1/115/senate/members.json',{headers: {'X-API-Key':'xt1aCaYtZjkGpJyZRi6DOiEYHCFmqIZlKb4zVAKO'}})
+      .then(data => data.json())
+      .then((data) => { this.setState({ members: data.results }) }); 
+  }
+
   render() {
     return (
-      <h1>Home Screen</h1>
+      <div>
+        <h1>Home Screen</h1>
+        
+        <div>
+        <ul>
+          {
+           this.state.members.map(function(item, i){
+             return(
+             item.members.map(function(m,j){
+              return <div key = {j}>{m.first_name} {m.last_name} </div>
+
+            })
+            )
+           })
+         }
+        </ul>
+      </div>
+        
+      </div>
     );
   }
 }
